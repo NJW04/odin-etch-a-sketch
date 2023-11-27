46c0420a59fe10f3b7ded2bb92c66c8f8d75114c
Nathan Wells <nthnwells1@gmail.com>
2023-11-27 20:18:17 +0200

    Finalized clear button and size restraints, added bonus rainbows ;)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # odin-etch-a-sketch
 A browser version of something between a sketchpad and an Etch-A-Sketch.
+It changes size dynamically depending on what size the user inputs, limited to 0-100 (not including 0, including 100).
+I also added a rainbow effect when hovering over squares, which is cooler!
--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const blockContainer = document.querySelector('.block-container');
 
 function populatePage(numBlocks){
-    //FIRST empty out block container
+    //First empty out block container
     while (blockContainer.firstChild) {
         blockContainer.removeChild(blockContainer.lastChild);
       }
@@ -10,14 +10,13 @@ function populatePage(numBlocks){
     for (let i=0; i<totalBlocks; i++){
         const square = document.createElement('div');
         square.classList.add('square');
-        //Also now calculate width and height for square
         const widthHeight = (960 / numBlocks) - 5;
         square.style.width = widthHeight + "px";
         square.style.height = widthHeight + "px";
 
         square.addEventListener('mouseover', () => {
-            //Do RGB random values here later
-            square.style.backgroundColor = 'black';
+            //Extra content rainbow colours
+            square.style.backgroundColor = 'rgb(' + randomRGBNumber() + ',' + randomRGBNumber() + ',' + randomRGBNumber() + ')';
         });
         
         blockContainer.append(square);
@@ -27,9 +26,29 @@ function populatePage(numBlocks){
 const pageButton = document.querySelector('#main-button');
 pageButton.addEventListener("click", () => {
     let num = prompt("Please enter the number of blocks you would like the grid to be (in range 0-100)");
-    populatePage(num);
+    if ((num <= 100) && (num > 0)){
+        populatePage(num);
+    }else{
+        alert("Please enter a valid NUMBER, from 0-100 (not including 0)");
+    }
+
 });
+
+const clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', clearBoard);
 
 window.onload = (event) => {
     populatePage(16);
   };
+
+  function randomRGBNumber(){
+    return Math.floor(Math.random() * 256);
+  }
+
+  function clearBoard(){
+    const allSquares = document.querySelectorAll('.square');
+    const squareArray = Array.from(allSquares);
+    for (const curSquare of squareArray) {
+        curSquare.style.backgroundColor = 'black';
+    }
+  }
